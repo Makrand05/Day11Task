13d182d435cb8952d29e6cb7e12850126fe3f0a8
Makrand <makrandshingare@gmail.com>
2021-11-13 09:40:31 +0530

    [maktand ] storing the data in dictonary

--- flipcoincomilator.sh
+++ flipcoincomilator.sh
@@ -1,2 +1,12 @@
-echo "hello git"
-
+declare -A singlet
+isTrue=true
+flipStatus=0
+counter=0
+while  [ $isTrue == true ]
+do
+	flipStatus=$((RANDOM%2))
+	singlet[ $counter ]=$flipStatus
+	read -p "Do you want to flip coin agin..(Y/N) " isTrue
+	((counter++))
+done
+echo "All values${singlet[@]}"
